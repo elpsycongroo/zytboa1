d1aa6d8cafd2c23fc329c678a0e9342715f66f31
Dominic <wobuxindelete@qq.com>
2017-03-21 16:57:28 +0800

    [201703211656]表格的用法

--- src/main/java/com/zytboa/controller/UserController.java
+++ src/main/java/com/zytboa/controller/UserController.java
@@ -2,18 +2,21 @@ package com.zytboa.controller;
 
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.HttpRequest;
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zytboa.service.UserService;
 import com.zytboa.vo.User;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -27,6 +30,16 @@ public class UserController {
 		List<User> userList = userServiceImpl.getAll();
 		model.addAttribute("userList",userList);
 		return "userList";
+	}
+
+	@RequestMapping("/userTable")
+	@ResponseBody
+	public Object userTable(int limit,int offset,String name){
+		Map<String,Object> resMap = new HashMap<>();
+		List<User> userList = userServiceImpl.getAll();
+		resMap.put("total",2);
+		resMap.put("rows",userList);
+		return JSON.toJSONString(resMap);
 	}
 	
 }
